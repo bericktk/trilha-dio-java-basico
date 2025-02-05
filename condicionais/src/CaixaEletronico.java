6b4d80ac9822e1398c132bd9febb3bb2bf2aa071
bericktk <br.ericktk@gmail.com>
2025-02-04 21:34:34 -0300

    adicionado o else

--- condicionais/src/CaixaEletronico.java
+++ condicionais/src/CaixaEletronico.java
@@ -1,11 +1,14 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 30.0;
 
         if(valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
             System.out.println("Saque realizado com sucesso!");
+            System.out.println("Saldo atual: " + saldo);
+        }else{
+            System.out.println("Saldo insuficiente!");
             System.out.println("Saldo atual: " + saldo);
         }
     }
